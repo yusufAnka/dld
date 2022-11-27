90c7a9e2b419cc34fccb63e9c102a87d520e03a5
yusuf <yusufanka54@gmail.com>
2022-11-27 22:53:09 +0100

    yes

--- src/components/ScreenCapture/ScreenCapture.jsx
+++ src/components/ScreenCapture/ScreenCapture.jsx
@@ -10,8 +10,8 @@ export default class ScreenCapture extends React.Component {
       const { cropPositionTop, cropPositionLeft, cropWidth, cropHeight } = {
         cropPositionTop: 0,
         cropPositionLeft: 0,
-        cropWidth: 860,
-        cropHeight: 860
+        cropWidth:  2400,
+        cropHeight: 1600
       };
       console.log("this.ref.current", this.ref.current)
   
